919365bcc4429a5848bdee039ba54a32de03d4e7
SuzanaMht <sujana.mehta@selu.edu>
2023-03-03 14:06:50 -0600

    adding-content zz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('../pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
@@ -13,8 +13,21 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('../pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'journal',
+    loadChildren: () => import('./pages/journal/journal.module').then( m => m.JournalPageModule)
+  },
+  {
+    path: 'article',
+    loadChildren: () => import('./pages/article/article.module').then( m => m.ArticlePageModule)
+  },
+  {
+    path: 'table',
+    loadChildren: () => import('./pages/table/table.module').then( m => m.TablePageModule)
   }
+
 
 ];
 
